f95599cb0e5e4c39b099e6b8d47fd8aa4a0273b5
Navya-Andra <anchowdary13@gmail.com>
2021-10-07 17:46:49 +0530

    changes to homepage

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
-import BooksList from './BooksList';
 import './Home.css'
 import News from './News'
+import books from './data.json';
+import {  Table, Divider } from "semantic-ui-react";
+
 class RecentBooks extends Component {
 	render() {
 	  return (
 		<div className="box-field">
 			<h3>My Recent Books</h3>
-	         <BooksList />
+			<div>
+			<Table singleLine>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Id</Table.HeaderCell>
+            <Table.HeaderCell>Title</Table.HeaderCell>
+            <Table.HeaderCell>Author</Table.HeaderCell>
+            <Table.HeaderCell>Year</Table.HeaderCell>
+			<Table.HeaderCell>Status</Table.HeaderCell>
+        </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {books.filter((books, id) => id < 5 ).map(el => {
+            return (
+              <Table.Row key={el.id}>
+                <Table.Cell>{el.id}</Table.Cell>
+                <Table.Cell>{el.title}</Table.Cell>
+                <Table.Cell>{el.author}</Table.Cell>
+                <Table.Cell>{el.year}</Table.Cell>
+				<Table.Cell>{el.status}</Table.Cell>
+        <Divider />
+			</Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+
+			</div>
 		</div>
 	  );
 	}
--- src/MyBooks.js
+++ src/MyBooks.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
-import {  Table, Divider } from "semantic-ui-react";
-import books from './data.json';
+import BooksList from './BooksList';
 import './Home.css'
 
 class Books extends Component {
@@ -10,35 +9,7 @@ class Books extends Component {
             <h1 style={{backgroundColor: "pink", margin: "20px", marginTop: "0px"}}>Requests</h1>
             <div className = "Books-field">
 			<h2>Request List</h2>
-	        <div>
-			<Table singleLine>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Id</Table.HeaderCell>
-            <Table.HeaderCell>Title</Table.HeaderCell>
-            <Table.HeaderCell>Author</Table.HeaderCell>
-            <Table.HeaderCell>Year</Table.HeaderCell>
-			<Table.HeaderCell>Status</Table.HeaderCell>
-        </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {books.filter((books, id) => id < 5 ).map(el => {
-            return (
-              <Table.Row key={el.id}>
-                <Table.Cell>{el.id}</Table.Cell>
-                <Table.Cell>{el.title}</Table.Cell>
-                <Table.Cell>{el.author}</Table.Cell>
-                <Table.Cell>{el.year}</Table.Cell>
-				<Table.Cell>{el.status}</Table.Cell>
-        <Divider />
-			</Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table>
-
-			</div>
+      <BooksList />
 		    </div>
 	    </div>
 	  );
